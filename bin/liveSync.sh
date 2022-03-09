9e3baa2994808ec6d26eb50e84a7fad676c8b644
Celian Ringwald <cringwal@prod-dbpedia-live.inria.fr>
2022-03-09 16:50:28 +0100

    use rather the last date of dump

--- bin/liveSync.sh
+++ bin/liveSync.sh
@@ -4,6 +4,9 @@
 MAIN_CLS="org.dbpedia.extraction.live.mirror.LiveSync"
 MVN="mvn"
 
+last_update=$(head -1 /appli/databus-data/last_update.txt)
 
+echo $last_update"-00-000000" > ../lastDownloadDate.dat;
+echo "========================="
 $MVN install exec:java -q -Dexec.mainClass="$MAIN_CLS" -Dexec.args="$*"
 
